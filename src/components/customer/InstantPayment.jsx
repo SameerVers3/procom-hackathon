594f0950752a5011fd3a49693c12ed5530e0b38c
Ali Nazir <alinazir105@gmail.com>
2024-03-06 15:33:02 +0500

    IP done

--- src/components/customer/InstantPayment.jsx
+++ src/components/customer/InstantPayment.jsx
@@ -3,70 +3,93 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { EyeIcon, EyeOffIcon } from "@heroicons/react/outline";
+import { Textarea } from "@/components/ui/textarea";
+
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Separator } from '@/components/ui/separator'
+
 
 
 const InstantPayment = () => {
-  const [showPassword, setShowPassword] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
-  return (
-    <div className="">
-      <div className="flex flex-col items-center justify-center h-screen gap-8">
-        <h1 className="text-2xl"><b>Instant Payment</b></h1>
-        <div>
-        <div className="flex gap-6">
-            <div className="w-48 flex justify-start flex-col gap-3">
-            <Label htmlFor="username" className="mr-auto">
-              Username
-            </Label>
-            <Input id="username" placeholder="Enter user name" />
-          </div>
-          <div className="w-64 flex justify-start flex-col gap-3">
-            <Label htmlFor="accountnumber" className="mr-auto">
-              Email Address(Optional)
-            </Label>
-            <Input id="accountnumber" placeholder="Enter your account number" />
-          </div>
-        </div>
-        <div className="mt-4 flex flex-col gap-5">
-          <div className=" flex justify-start flex-col gap-3">
-            <Label htmlFor="email" className="mr-auto">
-              Payment account
-            </Label>
-            <Input id="email" placeholder="abc@gmail.com" />
-          </div>
-          <div className=" flex justify-start flex-col gap-3">
-            <Label htmlFor="phone" className="mr-auto">
-              Phone
-            </Label>
-            <Input id="phone" placeholder="0123456789" />
-          </div>
-          <div className="flex justify-start flex-col gap-3 relative">
-            <Label htmlFor="password" className="mr-auto">
-              Password
-            </Label>
-            <Input
-              id="password"
-              type={showPassword ? "text" : "password"}
-              placeholder="Enter at least 8+ characters"
-            />
-            <button
-              onClick={togglePasswordVisibility}
-              className="absolute right-3 top-1/2 transform -translate-y-1/2"
-            >
-              {showPassword ? "Hide" : "Show"}
-            </button>
-          </div>
+    return (
+        <div className="">
+            <div className="flex flex-col items-center justify-center h-screen gap-8">
+                <h1 className="text-2xl"><b>Instant Payment</b></h1>
+                <div>
+                    <div className="flex gap-6">
+                        <div className="w-48 flex justify-start flex-col gap-3">
+                            <Label htmlFor="username" className="mr-auto">
+                                Username
+                            </Label>
+                            <Input id="username" placeholder="Enter user name" />
+                        </div>
+                        <div className="w-64 flex justify-start flex-col gap-3">
+                            <Label htmlFor="email" className="mr-auto">
+                                Email Address(Optional)
+                            </Label>
+                            <Input id="email" placeholder="Enter your email address" />
+                        </div>
+                    </div>
+                    <div className="mt-4 flex flex-col gap-5">
+                        <div className="flex justify-start flex-col gap-3">
+                            <Label htmlFor="paymentamount" className="mr-auto">
+                                Payment Amount
+                            </Label>
+                            <Input id="paymentamount" placeholder="abc@gmail.com" />
+                        </div>
+                        <div className=" flex justify-start flex-col gap-3">
+                            <Label htmlFor="merchantaccount" className="mr-auto">
+                                Merchant Account Number
+                            </Label>
+                            <Input id="merchantaccount" placeholder="0123456789" />
+                        </div>
 
-          <Button className="mt-4">Sign up</Button>
+                        <div className="">
+                            <Label htmlFor="bankname" className="mr-auto">
+                                Select Bank Name
+                            </Label>
+                            <Select>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Select a bank" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Banks</SelectLabel>
+                                        <SelectItem value="bankalhabib">Bank Al Habib</SelectItem>
+                                        <SelectItem value="meezanbank">Meezan Bank</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+
+                        <div className="border flex justify-start flex-col gap-3">
+                            <Label htmlFor="paymentpurpose" className="mr-auto">
+                                Payment Purpose
+                            </Label>
+                            <Textarea></Textarea>
+                        </div>
+                        <Button className="mt-4">Sign up</Button>
+                    </div>
+                </div>
+            </div>
         </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default InstantPayment;
